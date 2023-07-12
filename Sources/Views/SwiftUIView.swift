15b0682fca2389123679ec093d3d11f2313e053b
Allicia Viona <alliciawn@gmail.com>
2023-07-12 13:57:13 +0700

    - init project - Add SwiftUIView

---
+++ Sources/Views/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Allicia Viona Sagi on 12/07/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, Swict Package!")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
