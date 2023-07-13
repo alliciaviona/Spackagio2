c48e97754ede8f6e99ebc669fa5be52c277bb3f2
Allicia Viona <alliciawn@gmail.com>
2023-07-13 09:19:41 +0700

    - add os validation

--- Sources/Views/SwiftUIView.swift
+++ Sources/Views/SwiftUIView.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 public struct SwiftUIView: View {
     public init() {}
+    @available(iOS 13.0, *)
     public var body: some View {
         Text("Hello, Swict Package!")
     }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
+    @available(iOS 13.0, *)
     static var previews: some View {
         SwiftUIView()
     }
